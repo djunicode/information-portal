ec4e1fb65672c4923b3c3b8481fb66f7efd99db8
Varun Viswanath <91796995+maverick2903@users.noreply.github.com>
2023-04-24 23:40:53 +0530

    CRUD for Committees implemented (#32)
    Co-authored-by: Kartik Jolapara <42518907+codingmickey@users.noreply.github.com>

---
+++ backend/controllers/committee.controller.js
@@ -0,0 +1,65 @@
+const Committee = require('../models/committee.model');
+
+//add new committee
+const addCommittee = async (req, res) => {
+    try {
+        req.body.user_id = req.user._id;
+        let committee = new Committee(req.body);
+        await committee.save();
+        res.status(200).send({ message: 'New Committee Added!', status: 'successful' });
+    } catch (error) {
+        res.status(400).send({ message: error.message, status: 'failed' });
+    }
+};
+
+//view the committees
+const viewCommittees = async (req, res) => {
+    try {
+        const committees = await Committee.find({ user_id: req.user._id });
+        if (!committees) {
+            res.status(401).send({ message: 'No Committees present!', status: 'failed' });
+        } else {
+            res.status(200).send(committees);
+        }
+    } catch (error) {
+        res.status(400).send({ message: error.message, status: 'failed' });
+    }
+};
+
+//update committee
+const updateCommittee = async (req, res) => {
+    try {
+        const id = req.user._id;
+        const committee = req.body;
+        const committee_id = await Committee.findOne({
+            user_id: id,
+            committee_name: req.body.committee_name
+        });
+        const updatedCommittee = await Committee.findByIdAndUpdate(committee_id, committee, {
+            new: true
+        });
+        if (!updatedCommittee) {
+            res.status(401).send({ message: 'No Committees present!', status: 'failed' });
+        } else {
+            res.status(200).send(updatedCommittee);
+        }
+    } catch (error) {
+        res.status(400).send({ message: error.message, status: 'failed' });
+    }
+};
+
+//delete committee
+const deleteCommittee = async (req, res) => {
+    try {
+        const committee_id = await Committee.findOne(
+            { user_id: req.user._id, committee_name: req.params.name },
+            { _id: 1 }
+        );
+        await Committee.deleteOne({ _id: committee_id });
+        res.status(200).send({ message: 'Committee deleted successfully!', status: 'success' });
+    } catch (error) {
+        res.status(400).send({ message: error.message, status: 'failed' });
+    }
+};
+
+module.exports = { addCommittee, viewCommittees, updateCommittee, deleteCommittee };
--- backend/index.js
+++ backend/index.js
@@ -8,19 +8,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
 
 const authRouter = require('./routes/auth');
+
 const projectRouter = require('./routes/projectRoutes')
+const committeeRouter = require('./routes/committeeRoutes');
 // Assigning Routes
 app.use('/api/auth', authRouter);
 app.use('/api/project', projectRouter);
+app.use('/api/committee', committeeRouter);
 
 let port = process.env.PORT;
-if (port == null || port == "") {
-	port = 5000;
+if (port == null || port == '') {
+    port = 5000;
 }
 
 app.listen(port, () => {
-	console.log(`Server listening on ${port}`);
+    console.log(`Server listening on ${port}`);
 });
